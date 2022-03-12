2b48306b460f5069bf4c8ba2d076bb93c70daa03
LC <lrc3233@gmail.com>
2022-03-12 14:16:30 -0800

    add email

--- compare-availibilites.py
+++ compare-availibilites.py
@@ -1,12 +1,26 @@
 import urllib.request, json
 import datetime
 
+import sendgrid
+import os
+
+
+
 DATE_FORMAT = '%Y-%m-%dT%H:%M:%SZ'
 
 MONTH = '08'
 YEAR = '2022'
 
-CAMPSITES = {'259309': 'Lewis Lake, Yellowstone NP'}
+CAMPSITES = {
+  '232493': 'Fish Creek, Glacier NP',
+  '251869': 'Many Glacier, Glacier NP',
+  '232492': 'St. Mary, Glacier NP',
+  '234791': 'Devil Creek, NEAR Glacier NP',
+  '247663': 'Signal Mountain, Teton NP',
+  '247664': 'Jenny Lake, Teton NP',
+  '247785': 'Lizard Creek, Teton NP',
+  '258830': 'Colter Bay, Teton NP'
+}
 
 DATES_INTERESTED = [
   '2022-08-17T00:00:00Z',
@@ -49,7 +63,7 @@ for campsite_id, campsite_name in CAMPSITES.items():
       site_url = f"https://www.recreation.gov/camping/campsites/{id}"
       dt_times = [datetime.datetime.strptime(date, '%Y-%m-%dT%H:%M:%SZ') for date in entry]
       formatted_times = [date.strftime('%b %d') for date in dt_times]
-      line = (f"Campsite '{site}' in loop {loop} at {CAMPSITES[campsite_id]} is available for night of {formatted_times}. ({site_url})")
+      line = (f"Campsite '{site}' in loop {loop} at {CAMPSITES[campsite_id]} is available for night of {formatted_times}. (<a href='{site_url}'>link</a>)")
       email_text.append(line)
         # print(availabilities[date])
     # import ipdb; ipdb.set_trace()
@@ -57,3 +71,31 @@ for campsite_id, campsite_name in CAMPSITES.items():
 
 print(email_text)
   # "Campsite [site (c095)] [(id ___)] at [campsite name] is available for night of ________ "
+
+
+sg = sendgrid.SendGridAPIClient(api_key=os.environ.get('SENDGRID_API_KEY'))
+data = {
+  "personalizations": [
+    {
+      "to": [
+        {
+          "email": "lrc3233@gmail.com"
+        }
+      ],
+      "subject": "New campground availibilities"
+    }
+  ],
+  "from": {
+    "email": "lrc3233@gmail.com"
+  },
+  "content": [
+    {
+      "type": "text/plain",
+      "value": '\n'.join(email_text)
+    }
+  ]
+}
+response = sg.client.mail.send.post(request_body=data)
+print(response.status_code)
+print(response.body)
+print(response.headers)
