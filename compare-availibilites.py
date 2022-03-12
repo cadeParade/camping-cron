c948dcfe17634dda23afd335b10cb89e88fefa6b
LC <lrc3233@gmail.com>
2022-03-12 10:30:04 -0800

    basic json parsing

---
+++ compare-availibilites.py
@@ -0,0 +1,59 @@
+import urllib.request, json
+import datetime
+
+DATE_FORMAT = '%Y-%m-%dT%H:%M:%SZ'
+
+MONTH = '08'
+YEAR = '2022'
+
+CAMPSITES = {'259309': 'Lewis Lake, Yellowstone NP'}
+
+DATES_INTERESTED = [
+  '2022-08-17T00:00:00Z',
+  '2022-08-18T00:00:00Z',
+  '2022-08-19T00:00:00Z',
+  '2022-08-20T00:00:00Z',
+  '2022-08-21T00:00:00Z',
+  '2022-08-22T00:00:00Z',
+  '2022-08-23T00:00:00Z',
+  '2022-08-24T00:00:00Z',
+  '2022-08-25T00:00:00Z',
+  '2022-08-26T00:00:00Z',
+  '2022-08-27T00:00:00Z',
+  '2022-08-28T00:00:00Z',
+  '2022-08-29T00:00:00Z',
+]
+
+email_text = []
+
+
+for campsite_id, campsite_name in CAMPSITES.items():
+  with urllib.request.urlopen(
+    f"https://www.recreation.gov/api/camps/availability/campground/{campsite_id}/month?start_date={YEAR}-{MONTH}-01T00%3A00%3A00.000Z"
+  ) as url:
+    data = json.loads(url.read().decode())['campsites']
+
+    dates_available_for_sites = {}
+    for site_id, site_data in data.items():
+      site_availabilities = site_data['availabilities']
+      for date in DATES_INTERESTED:
+        if site_availabilities.get(date) == 'Available':
+          if site_id in dates_available_for_sites:
+            dates_available_for_sites[site_id].append(date)
+          else:
+            dates_available_for_sites[site_id] = [date]
+
+    for id, entry in dates_available_for_sites.items():
+      site = data[id]['site']
+      loop = data[id]['loop']
+      site_url = f"https://www.recreation.gov/camping/campsites/{id}"
+      dt_times = [datetime.datetime.strptime(date, '%Y-%m-%dT%H:%M:%SZ') for date in entry]
+      formatted_times = [date.strftime('%b %d') for date in dt_times]
+      line = (f"Campsite '{site}' in loop {loop} at {CAMPSITES[campsite_id]} is available for night of {formatted_times}. ({site_url})")
+      email_text.append(line)
+        # print(availabilities[date])
+    # import ipdb; ipdb.set_trace()
+    # print(data)
+
+print(email_text)
+  # "Campsite [site (c095)] [(id ___)] at [campsite name] is available for night of ________ "
